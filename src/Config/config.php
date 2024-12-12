971a8b190b660ae0e27f490ec53f5a5a1616b6a3
Alebro112 <sevas.bl@gmail.com>
2024-12-12 03:32:03 +0200

    mariadb v1

--- src/Config/config.php
+++ src/Config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 // Database Config
-define("DB_HOST", "localhost:3308");
+define("DB_HOST", "localhost:3306");
 define("DB_USER", "betting");
 define("DB_PASS", "betting");
 define("DB_NAME", "betting");
