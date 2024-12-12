62125b6b32293907c35b40772a03bb729724a38c
Alebro112 <sevas.bl@gmail.com>
2024-12-12 02:24:12 +0200

    dockernginx v5.16

--- src/Config/config.php
+++ src/Config/config.php
@@ -1,13 +1,13 @@
 <?php
 
 // Database Config
-define("DB_HOST", "");
+define("DB_HOST", "localhost:3308");
 define("DB_USER", "");
 define("DB_PASS", "");
 define("DB_NAME", "");
 
 // Base URL
-define("BASEURL", "http://localhost");
+define("BASEURL", "");
 
 
 
